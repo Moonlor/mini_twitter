27ccc12c79b2aef7b28d9b855eafd6f3255a20d1
Moonlor <Kenneth0875@gmail.com>
2017-12-13 20:19:02 +0800

    修改模板，DRY

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Mini Twitter')
     end 
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
     
   end
 
@@ -17,6 +22,12 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
 	  end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end
+
   end
 
   describe "About page" do
@@ -25,6 +36,12 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About Us")
+    end
+
   end
 
 end
